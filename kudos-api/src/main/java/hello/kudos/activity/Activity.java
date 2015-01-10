6136f3722100004d052840f3a1a174d44269e15a
Juan Fernández-Corugedo Igual <juan.fernandez-corugedo.contractor@bbva.com>
2015-01-10 12:12:38 +0100

    Static folder added

--- kudos-api/src/main/java/hello/kudos/activity/Activity.java
+++ kudos-api/src/main/java/hello/kudos/activity/Activity.java
@@ -17,11 +17,11 @@ public class Activity implements Serializable{
     private int kudosIn = 0;
     private int kudosOut = 0;
     
-    public TeamMember(){
+    public Activity(){
     	
     }
     
-	public TeamMember(String nick, String name, int kudosIn, int kudosOut) {
+	public Activity(String nick, String name, int kudosIn, int kudosOut) {
 		super();
 		this.nick = nick;
 		this.name = name;
@@ -57,29 +57,6 @@ public class Activity implements Serializable{
 		return ReflectionToStringBuilder.toString(this);
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nick == null) ? 0 : nick.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TeamMember other = (TeamMember) obj;
-		if (nick == null) {
-			if (other.nick != null)
-				return false;
-		} else if (!nick.equals(other.nick))
-			return false;
-		return true;
-	}
+	
 	
 }
